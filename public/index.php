7af17411afc967dffae3682587ff278ce8b784c7
RyotArch <bboy.ryu@icloud.com>
2018-06-17 19:40:08 -0700

    [Add] create public/index.php frontpage and addional webtools

---
+++ public/index.php
@@ -0,0 +1,60 @@
+<?php
+
+use Phalcon\Loader;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Url as UrlProvider;
+use Phalcon\Mvc\Application;
+
+
+$loader = new Loader();
+
+$loader->registerDirs(
+    [
+        "../app/controllers/",
+        "../app/models/",
+    ]
+);
+
+$loader->register();
+
+
+// Create a DI
+$di = new FactoryDefault();
+
+// Setup the view component
+$di->set(
+    "view",
+    function () {
+        $view = new View();
+
+        $view->setViewsDir("../app/views/");
+
+        return $view;
+    }
+);
+
+// Setup a base URI so that all generated URIs include the "tutorial" folder
+$di->set(
+    "url",
+    function () {
+        $url = new UrlProvider();
+
+        $url->setBaseUri("/tutorial/");
+
+        return $url;
+    }
+);
+
+
+$application = new Application($di);
+
+try {
+    // Handle the request
+    $response = $application->handle();
+
+    $response->send();
+} catch (\Exception $e) {
+    echo "Exception: ", $e->getMessage();
+}
+
